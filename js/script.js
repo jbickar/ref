e99b2777420d5a004f2497b18901a9e1e7a6b9f6
John Bickar <jbickar@stanford.edu>
2011-04-20 12:40:20 -0700

    Click incrementing works!!!

--- js/script.js
+++ js/script.js
@@ -3,7 +3,18 @@
 //$(alert("Hello, World!"));
 
 $(document).ready(function() {
-  $("#field-ref-txn-dir-person-add-more-wrapper").css("background-color","red");
+/*  $(".group-ref-txn-dir input.form-text,.group-ref-txn-ref input.form-text,.group-ref-txn-tech input.form-text").css({
+    "background-color": "#990000",
+    "color": "#FFFFFF"
+  }); */
+    $(".group-ref-txn-dir input.form-text,.group-ref-txn-ref input.form-text,.group-ref-txn-tech input.form-text").click(function(){
+      var oldValue = $(this).val();
+      if (!oldValue) {
+        var oldValue = 0;
+      }
+   	  var newVal = parseFloat(oldValue) + 1;
+   	  $(this).val(newVal);
+    });
 });
 
 /*$(document).ready(function() {
