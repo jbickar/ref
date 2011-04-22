f57cd9e9729ce394748d30240a058f7072e05daa
John Bickar <jbickar@stanford.edu>
2011-04-22 15:44:42 -0700

    Tweaks to CSS Moved increment JS function to the module Added CSS background images

--- js/script.js
+++ js/script.js
@@ -1,24 +1,13 @@
 //$Id$
 (function($) { 
-//$(alert("Hello, World!"));
 
-$(document).ready(function() {
-/*  $(".group-ref-txn-dir input.form-text,.group-ref-txn-ref input.form-text,.group-ref-txn-tech input.form-text").css({
-    "background-color": "#990000",
-    "color": "#FFFFFF"
-  }); */
-    $(".group-ref-txn-dir input.form-text,.group-ref-txn-ref input.form-text,.group-ref-txn-tech input.form-text").click(function(){
-      var oldValue = $(this).val();
-      if (!oldValue) {
-        var oldValue = 0;
-      }
-   	  var newVal = parseFloat(oldValue) + 1;
-   	  $(this).val(newVal);
-    });
-});
-
-/*$(document).ready(function() {
- alert($('field-ref-txn-dir-person-add-more-wrapper').length + ' elements!'); 
-});*/ 
+  $(document).ready(function() {
+	/*  $(".group-ref-txn-dir input.form-text,.group-ref-txn-ref input.form-text,.group-ref-txn-tech input.form-text").css({
+			"background-color": "#990000",
+			"color": "#FFFFFF"
+		}); */
+// Skip the following for now but will be a good way to swap out text for images and maintain accessibility
+//		$("#ref-txn-node-form fieldset label").addClass("element-invisible");
+	});
 
 })(jQuery);
